f43306502e04698829f91488be719a0a5365d504
Rebecca <rebecca.eve.cohen@gmail.com>
2020-12-13 11:23:23 -0700

    template project.  Vscode setup legit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Enter a number, please\n";
+	int x;
+	// std::cin >> x;
+	x = 23;
+	std::cout << x << " squared equals " << x * x << std::endl;
+	return 0;
+}
